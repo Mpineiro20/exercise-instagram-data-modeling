6e485b70bb4869d5731d2eb705d63e08915bddc6
Mpineiro20 <130186047+Mpineiro20@users.noreply.github.com>
2023-09-18 22:19:09 +0000

    second try

--- src/models.py
+++ src/models.py
@@ -1,56 +1,135 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+import datetime
 
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    username = Column(String(250), nullable=False, unique=True)
-    firstname = Column(String(250), nullable=True)
-    lastname = Column(String(250), nullable=True)
-    email = Column(String(250), nullable=False, unique=True)
-    followers = relationship("Follower", back_populates="user_from")
-    posts = relationship("Post", back_populates="user")
+    user_name = Column (String(250), unique = True, nullable = False)
+    password = Column(String(250), nullable=False)
+    email =  Column (String(250), unique = True, nullable = False)
+    full_name =  Column (String(250), nullable = False)
+    bio  = Column (String(300),nullable = True)
+    profile_picture = Column(String)
+    post = relationship ('Post', back_populates = 'user')
+    follower = relationship ('Follower' , back_populates = 'follower' , lazy ='dynamic')
+    following = relationship ('Following', back_populates = 'following' , lazy = 'dynamic')
 
-class Follower(Base):
-    __tablename__ = 'follower'
-    id = Column(Integer, primary_key=True)
-    user_from_id = Column(Integer, ForeignKey('user.id'))
-    user_to_id = Column(Integer, ForeignKey('user.id'))
-    user_from = relationship("User", foreign_keys=[user_from_id], back_populates="followers")
-    user_to = relationship("User", foreign_keys=[user_to_id])
-
+    def serialize (self):
+        return{
+            'user_name' : self.user_name,
+            'email' : self.email,
+            'full_name' : self. full_name,
+            'bio' : self.bio,
+            'profile_picture' : self.profile_picture
+        }
+   
 class Post(Base):
-    __tablename__ = 'post'
+    __tablename__ = 'posts'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    user = relationship("User", back_populates="posts")
-    media = relationship("Media", back_populates="post")
-    comments = relationship("Comment", back_populates="post")
+    user_id = Column (Integer, ForeignKey('users.id'))
+    image_url = Column (String(150), nullable = False)
+    caption = Column (String(300), nullable = True)
+    location = Column (String(300), nullable = True)
+    post_time = Column(DateTime, default = datetime.datetime.utcnow)
+    user = relationship('User' ,back_populates = 'post')
+    comment = relationship ('Comment', back_populates = 'user')
 
-class Media(Base):
-    __tablename__ = 'media'
-    id = Column(Integer, primary_key=True)
-    url = Column(String, nullable=False)
-    post_id = Column(Integer, ForeignKey('post.id'))
-    post = relationship("Post", back_populates="media")
 
-class Comment(Base):
-    __tablename__ = 'comment'
-    id = Column(Integer, primary_key=True)
-    author_id = Column(Integer, ForeignKey('user.id'))
-    post_id = Column(Integer, ForeignKey('post.id'))
-    author = relationship("User")
-    post = relationship("Post")
+    def to_dict(self):
+        return {
+            'user_id' : self.user_id,
+            'image_url' : self.image_url,
+            'caption': self. caption,
+            'location' : self.location,
+            'post_time': self.post_time
+        }
+    
 
+class Comment (Base):
+    __tablename__ = 'comments'
+    id = Column (Integer , primary_key = True)
+    user_id = Column (Integer, ForeignKey('users.id'))
+    post_id = Column (Integer, ForeignKey('posts.id'))
+    text_comment = Column (String (250), nullable = False)
+    comment_time = Column (DateTime , default = datetime.datetime.utcnow)
+    user = relationship ('User')
+    post = relationship ('Post', back_populates = 'comment')
+    like = relationship ('Like', back_populates ='user')
+
+    def serialize (self):
+        return{
+            'user_id': self.user_id,
+            'post_id': self.post_id,
+            'text_comment': self.text_comment,
+            'comment_time': self.comment_time
+        }
+
+
+
+class Like (Base):
+    __tablename__ = 'likes'
+    id = Column (Integer, primary_key = True)
+    user_id = Column (Integer , ForeignKey ('users.id'))
+    post_id = Column (Integer, ForeignKey('posts.id'))
+    count_likes = Column (Integer)
+    like_time = Column (DateTime, default = datetime.datetime.utcnow)
+    post = relationship ('Post' , back_populates = 'like')
+    user = relationship ('User')
+    
+
+    def serialize (self):
+        return{
+            'user_id': self.user_id,
+            'post_id' : self.post_id,
+            'count_likes': self.count_likes,
+            'like_time' : self.like_time
+        }
+
+class Follower (Base) :
+    __tablename__ = 'followers'
+    id = Column (Integer , primary_key = True)
+    user_id = Column (Integer , ForeignKey ('users.id'))
+    follower_time = Column (DateTime , default = datetime.datetime.utcnow)
+    count_followers = Column (Integer)
+    user = relationship ('User' , back_populates = 'follower')
+
+    def serialize (self):
+        return{
+            'user_id': self.user_id,
+            'followe_time' : self.follower_time,
+            'count_followers' : self.count_followers
+        }
+
+class Following (Base):
+    __tablename__ ='followings'
+    id = Column (Integer, primary_key = True)
+    user_id = Column (Integer, ForeignKey ('users.id'))
+    count_following = Column (Integer)
+    following_time = Column (DateTime, default = datetime.datetime.utcnow)
+    user = relationship ('User' , back_populates = 'following')
+
+    def serialize (self):
+        return{
+            'user_id' : self.user_id,
+            'count_following' : self.count_following,
+            'following_time' : self. following_time
+        }
+
+## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
     print("Success! Check the diagram.png file")
 except Exception as e:
-    print("There was a problem generating the diagram")
+    print("There was a problem genering the diagram")
     raise e
